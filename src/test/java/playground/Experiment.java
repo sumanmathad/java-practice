b0718c552527c714dc2784948f7a41407409285c
suman_mathad <suman.mathad@gmail.com>
2023-08-25 15:51:18 +0530

    java updates

---
+++ src/test/java/playground/Experiment.java
@@ -0,0 +1,21 @@
+package playground;
+/*
+This is a playground for java code. May be deleted
+ */
+public class Experiment {
+    public static void main(String[] args) {
+        String name = "suman";
+        Integer numberone = new Integer(32);
+        Integer numbertwo = new Integer(32);
+        System.out.println(name+numberone);
+
+        Integer a = 32;
+        Integer b =32;
+        System.out.println(a==b);
+        System.out.println(numberone==numbertwo);
+
+
+    }
+
+
+}

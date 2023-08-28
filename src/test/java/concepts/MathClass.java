1dced6ce994cacacf39f82019c2622bf611e9efc
suman_mathad <suman.mathad@gmail.com>
2023-08-28 21:10:56 +0530

    java updates

--- src/test/java/concepts/MathClass.java
+++ src/test/java/concepts/MathClass.java
@@ -5,18 +5,36 @@ import org.junit.jupiter.api.Test;
 public class MathClass {
 
     @Test
-    public void MoreThanIntMax(){
-        int ThreeLessThanMax = Integer.MAX_VALUE-3;
-        for(int i=0;i<=6;i++){
+    public void MoreThanIntMax() {
+        int ThreeLessThanMax = Integer.MAX_VALUE - 3;
+        for (int i = 0; i <= 6; i++) {
             System.out.println(ThreeLessThanMax);
             ThreeLessThanMax++;
         }
         // program overflows
         //Avoid overflow with incrementExact and throw an exception
         int newThreeLessThanMax = Integer.MAX_VALUE;
-        for(int i=0, id =newThreeLessThanMax ; i<=6;Math.incrementExact(newThreeLessThanMax),i++){
+        for (int i = 0, id = newThreeLessThanMax; i <= 6; Math.incrementExact(newThreeLessThanMax), i++) {
             System.out.println(newThreeLessThanMax);
             ThreeLessThanMax++;
         }
     }
+
+    @Test
+    public void MathAbsolute(){
+        System.out.println(Math.abs(-0.2));
+        //returns double
+        System.out.println(Math.abs(Integer.MIN_VALUE));
+        //Overflow happens
+        //System.out.println(Math.absExact(Integer.MIN_VALUE));
+        //exception - works with language level 15 textblocks
+        System.out.println(Math.abs((long)Integer.MIN_VALUE));
+
+        System.out.println(Math.abs(-3.8951));
+        double d= Math.abs(-3.3);
+        System.out.println(d);
+    }
+
+
 }
+
--- src/test/java/playground/Experiment.java
+++ src/test/java/playground/Experiment.java
@@ -4,24 +4,11 @@ This is a playground for java code. May be deleted
  */
 public class Experiment {
     public static void main(String[] args) {
-/*        String name = "suman";
-        Integer numberone = new Integer(32);
-        Integer numbertwo = new Integer(32);
-        System.out.println(name+numberone);
+      String name = "suman";
+      String abc= new String("test");
+      String d =abc.intern();
+        System.out.println(name.repeat(2));
 
-        Integer a = 32;
-        Integer b =32;
-        System.out.println(a==b);
-        System.out.println(numberone==numbertwo);*/
-        int maxnum = Integer.MAX_VALUE;
-        int maxnumminusFive=Integer.MAX_VALUE-5;
-
-        System.out.printf("%,d%n",maxnum);
-        System.out.printf("%d",maxnumminusFive);
-        for (int i=0;i<10;i++){
-            System.out.printf("%,d%n",maxnumminusFive);
-            maxnumminusFive++;
-        }
 
     }
 
